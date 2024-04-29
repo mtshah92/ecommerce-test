7fc71b6efce78a9f82740481965bb62a7831437c
mtshah92 <mtshah92@gmail.com>
2024-04-29 16:35:35 +0530

    route updated

--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
 export const PrivateRoute = ({ children }) => {
   const isAuthenticated = localStorage.getItem("token");
-  return isAuthenticated ? children : <Navigate to="/login" />;
+  return isAuthenticated ? children : <Navigate to="/" />;
 };
